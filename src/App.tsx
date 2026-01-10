58d219f05f3d6147e2dbba9d66ace7fd1b3e403e
Greg177 <szepgergo13@gmail.com>
2026-01-10 01:03:15 +0100

    I created my own navbar-like tabs menu replaced the old navbar style to mine also minor fix in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Container from "react-bootstrap/Container";
-//import AppNavbar from "./components/AppNavbar"; // There was no navbar in the illustration I used it for testing purpose only
 import Movies from "./pages/Movies";
 import Sandbox from "./pages/Sandbox";
 
